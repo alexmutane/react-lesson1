555fbfbf46da5ffe1fb7709b77ee16d32f7894db
alex <alex@equipmoz.org>
2022-10-12 15:25:11 +0200

    the react main concepts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Age: {props.age}</h2>
+      <h4>Work: {props.work}</h4>
+    </>
+  )
+}
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    setCounter(10);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name="Alex" age="30" work="Project-Manager" />
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      {counter}
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
     </div>
   );
 }
