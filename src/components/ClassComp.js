972a6bce049efa84d714d09cca3a24429bdfcddb
alex <alex@equipmoz.org>
2022-10-14 11:33:41 +0200

    class components

--- src/components/ClassComp.js
+++ src/components/ClassComp.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+//1. it extends the component class
+//2. it must have a render function that returns a jsx element
+//3. it exports the class to be available outside itself
 class Usingclass extends React.Component {
   render() {
     return <h2>using class component</h2>
