efac013b3b9f7ed38193ee958f73f920cf99e83d
alex <alex@equipmoz.org>
2022-10-17 20:49:17 +0200

    props and state destructing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 
 import Message from './components/States';
 import Counter from './components/Counter';
+import DestrcutProps from "./components/Props";
+import DestrcutPropsBody from './components/Props';
 
 const App = () => {
 
   return (
     <div className="App">
       {/* <Message /> */}
-      <Counter />
+      {/* <Counter /> */}
+      <DestrcutProps name="John" profession="Coder" />
+      <DestrcutPropsBody name="Doe" profession="Engineer" />
     </div>
   );
 }
--- src/components/Props.js
+++ src/components/Props.js
@@ -15,13 +15,28 @@ const UsingProps = (props) => {
 //PROPS.CHILDREN is only rendered where comps has children
 export class PropsClass extends React.Component {
   render() {
+    //or use Destructing, like below
+    // const { name, profession } = this.props;
+    //then in JSX call variables directly, without "this.props"
     return (
       <div style={{ border: '1px solid #ccc', padding: '3px', marginTop: '4px' }}>
         <h3>Props using class. {this.props.name}</h3>
         <p>{this.props.children}</p>
       </div>
     )
+    //it's the same to STATE
+    //const {state1, state2} = this.state
   }
+}
+
+//destruct props to use access them directly, without object.
+export const DestrcutProps = ({ name, profession }) => {
+  return <h5>Name: {name}, Profession: {profession}</h5>
+}
+
+export const DestrcutPropsBody = props => {
+  const { name, profession } = props;
+  return <h5>Worker: {name}. He is: {profession}</h5>
 }
 
 const fewStyle = {
