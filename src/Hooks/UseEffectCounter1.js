718a811826637250bb2cee2508dfef026a07b72b
alex <alexmutane@live.com>
2022-11-05 15:26:11 +0200

    use-effect with condition

--- src/Hooks/UseEffectCounter1.js
+++ src/Hooks/UseEffectCounter1.js
@@ -2,12 +2,19 @@ import React, { useEffect, useState } from 'react'
 
 function UseEffectCounter1() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState('');
+
   useEffect(() => {
+    console.log('updatng....');
     document.title = `cliked ${count}`;
-  })
+  }, [count])
+
   return (
     <div>
       <button onClick={() => setCount(count + 1)}>clicks {count}</button>
+      <p>
+        <input name='text' value={name} onChange={(ev) => setName(ev.target.value)} />
+      </p>
     </div>
   )
 }
