18837fc69050bd32596fe5e749b7c788a1ab5ecc
alex <alexmutane@live.com>
2022-11-05 17:14:42 +0200

    use-effect, fetch data on button click

--- src/Hooks/DataFetching.js
+++ src/Hooks/DataFetching.js
@@ -5,16 +5,17 @@ const DataFetching = () => {
   const [posts, setPosts] = useState([])
   const [id, setId] = useState(1)
   const [post, setPost] = useState('')
+  const [idButton, setIdButton] = useState(1)
 
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .get(`https://jsonplaceholder.typicode.com/posts/${idButton}`)
       .then(res => {
         console.log('one', res)
         setPost(res.data);
       })
       .catch(err => console.log(err))
-  }, [id])
+  }, [idButton])
 
   useEffect(() => {
     axios
@@ -25,9 +26,14 @@ const DataFetching = () => {
       .catch(err => console.log(err))
   }, [])
 
+  const handleFetchData = () => {
+    setIdButton(id)
+  }
+
   return (
     <div>
       <input type='text' value={id} onChange={ev => setId(ev.target.value)} />
+      <button onClick={handleFetchData}>Fetch</button>
       {/* <ul>
         {posts.map(post => <li key={post.id}>{post.title}</li>)}
       </ul> */}
